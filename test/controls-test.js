f86670bbc75e9b259d43005ab7ea74b21864d200
David Millet <david.millet@gmail.com>
2016-09-14 16:12:33 -0700

    expanding out the controls test a bit

--- test/controls-test.js
+++ test/controls-test.js
@@ -21,12 +21,35 @@ function updateState() {
 	stream1.updateNow({count: state.count});
 }
 
-function outputState(update) {
+function outputUpdate(update) {
 	var delay = now() - timeLogs[update.count];
 	console.info(
 		now() + ':',
 		'received update with a delay of ' + delay + ' ms:',
 		JSON.stringify(update)
+	);
+}
+
+function outputState(state) {
+	var delay = now() - timeLogs[state.update.count];
+	console.info(
+		now() + ':',
+		'received state with a delay of ' + delay + ' ms:',
+		JSON.stringify(state)
+	);
+}
+
+function outputDataBurst(burst) {
+	console.info(
+		now() + ':',
+		'----received a burst----:'
+	);
+	burst.forEach(function(gameState) {
+		//outputUpdate(gameState.update);
+		outputState(gameState);
+	});
+	console.info(
+		'------------------------'
 	);
 }
 
@@ -36,7 +59,8 @@ var stream1 = new GameStream({
 });
 
 var stream2 = new GameStream();
-stream2.on('update', outputState);
+stream2.on('update', outputUpdate);
+//stream2.on('data', outputDataBurst);
 stream1.pipe(stream2);
 
 setInterval(updateState, UPDATE_INTERVAL_MS);
