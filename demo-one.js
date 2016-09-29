ee40f9d0522702198aa5a39100a7f52d7a4da5d0
David Millet <dmillet@box.com>
2016-09-28 20:19:22 -0700

    Updates

--- demo-one.js
+++ demo-one.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/gamestream/build/";
+/******/ 	__webpack_require__.p = "/gamestream/build/build/";
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
